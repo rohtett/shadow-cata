9383f5d0a907cfb575de8b2d78344e69941eb771
Rohit <raheet@hotmail.co.uk>
2022-07-13 16:55:04 +0100

    stats page completed

--- src/main.js
+++ src/main.js
@@ -1,7 +1,30 @@
 "use strict";
 (()=> {
-  angular.module("main", [])
-  .controller("headerController", ($scope) => {
-    $scope.menus = ["Introduction", "Talents", "Rotation", "Stat Priority"];
-  });
+  angular.module("main", ["ngRoute"])
+  //routes
+  .config(["$routeProvider", function($routeProvider) {
+    $routeProvider
+      .when("/introduction", {
+        templateUrl: "app/introduction.html"
+      })
+      .when("/talents", {
+        templateUrl: "app/talents.html"
+      })
+      .when("/rotation", {
+        templateUrl: "app/rotation.html"
+      })
+      .when("/stats", {
+        templateUrl: "app/stats.html"
+      })
+      .otherwise({
+        redirectTo: "/introduction"
+      });
+  }])
+  //header controller
+  .controller("headerController", ["$scope", function($scope) {
+    $scope.menus = ["introduction", "talents", "rotation", "stats"];
+    $scope.locateTo = menu => {
+      window.location.href='#!/'+menu;
+    }
+  }]);
 })();
